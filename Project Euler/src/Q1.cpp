aa69b46c08b12f279cc261b776df37ec7ee064b3
Manithj <marapperuma1@gmail.com>
2023-02-22 21:48:02 +0530

    added Q1

---
+++ Project Euler/src/Q1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Q1.h"
+
+using namespace std;
+
+int main() {
+    cout << sum() << endl;
+    return 0;
+}
+
+
---
+++ Project Euler/src/Q1.h
@@ -0,0 +1,10 @@
+#pragma once
+int sum() {
+    int sum = 0;
+    for (int i = 1; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
